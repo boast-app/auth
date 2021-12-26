965e9c430fb1857f0d5d68d8fb45eb05cdddaa6b
Cosmic-rare <ysolakey1116@gmail.com>
2021-12-26 20:43:27 +0900

    TOKENの検証のエンドポイントを追加

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,13 +2,27 @@ const express = require("express")
 const router = express.Router()
 const User = require("../models/user.model")
 
-router.get("/", async (req, res) => {
-  try {
-    const users = await User.find()
-    res.json(users)
-  } catch (err) {
-    res.status(500).json({ message: err.message})
-  }
+const admin = require("firebase-admin")
+const serviceAccount = require("./firebase_key.json")
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
+
+router.post("/verify", (req, res) => {
+  const token = req.body.token
+  admin.auth().verifyIdToken(token)
+    .then((decodedToken) => {
+      res.json({
+        ok: true,
+        data: decodedToken
+      })
+    })
+    .catch((err) => {
+      res.json({
+        ok: false,
+        message: err.code
+      })
+    })
 })
 
 module.exports = router
